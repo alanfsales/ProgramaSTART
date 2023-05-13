f21d62ccf45fb5451afd9fef7e44af3a1654adaf
AlanFSales <alanfsales27@gmail.com>
2023-05-12 22:42:06 -0300

    Até a aula 4A Angular

---
+++ Trilha Java/05 - Angular/fundamentos-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponenteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
