05896761f40404b2c529699d5c16f6a151e8d21c
AlanFSales <alanfsales27@gmail.com>
2023-05-13 22:36:01 -0300

    Até a aula 4D Angular

--- Trilha Java/05 - Angular/fundamentos-angular/src/app/app.module.ts
+++ Trilha Java/05 - Angular/fundamentos-angular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
