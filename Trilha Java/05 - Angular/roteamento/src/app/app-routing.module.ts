123a6bd5498a214d6005cd188173a89b4490d137
AlanFSales <alanfsales27@gmail.com>
2023-05-18 22:54:12 -0300

    Até a aula 5F 7/35 Aungular

--- Trilha Java/05 - Angular/roteamento/src/app/app-routing.module.ts
+++ Trilha Java/05 - Angular/roteamento/src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent},
-  {path: "segunda-pagina", component: SegundaPaginaComponent},
-  {path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
-  {path: "**", component: PaginaNaoEncontradaComponent}
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent},
+  { path: "segunda-pagina", component: SegundaPaginaComponent},
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [AuthGuard]},
+  { path: "login", component: LoginComponent},
+  { path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
+  { path: "**", component: PaginaNaoEncontradaComponent}
+  
+
 ]
 
 @NgModule({
---
+++ Trilha Java/05 - Angular/roteamento/src/app/pagina-protegida/pagina-protegida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-protegida',
+  templateUrl: './pagina-protegida.component.html',
+  styleUrls: ['./pagina-protegida.component.css']
+})
+export class PaginaProtegidaComponent {
+
+}
