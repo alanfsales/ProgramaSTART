eb14f7db87f78223f085af4792c40530485cebcf
AlanFSales <alanfsales27@gmail.com>
2023-05-22 22:26:25 -0300

    Até a aula 6H Angular

--- Trilha Java/05 - Angular/proway-computers/src/app/produtos/produtos.component.ts
+++ Trilha Java/05 - Angular/proway-computers/src/app/produtos/produtos.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
+import { ProdutosService } from '../produtos.service';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent {
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto[] | undefined;
 
-  produtos: IProduto[] = produtos;
+  constructor(
+    private produtoService: ProdutosService
+  ){}
+
+  ngOnInit(): void{
+    this.produtos = this.produtoService.getAll();
+  }
 
 }
