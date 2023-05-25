99827e90b60b24669685b63a5437f7176c8faf8e
AlanFSales <alanfsales27@gmail.com>
2023-05-24 22:46:53 -0300

    Até aula 6J Angular

--- Trilha Java/05 - Angular/proway-computers/src/app/app.module.ts
+++ Trilha Java/05 - Angular/proway-computers/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NaoEncotradaComponent } from './nao-encotrada/nao-encotrada.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { NaoEncotradaComponent } from './nao-encotrada/nao-encotrada.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Trilha Java/05 - Angular/proway-computers/src/app/header/header.component.ts
+++ Trilha Java/05 - Angular/proway-computers/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CarrinhoService } from '../carrinho.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(
+    public carrinhoService: CarrinhoService
+  ){}
 }
