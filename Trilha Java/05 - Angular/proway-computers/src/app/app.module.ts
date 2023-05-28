b2408295c6a646ce8676ebfaa7776b708862e79f
AlanFSales <alanfsales27@gmail.com>
2023-05-27 23:34:24 -0300

    Até a aula 7C Angular

--- Trilha Java/05 - Angular/proway-computers/src/app/app.module.ts
+++ Trilha Java/05 - Angular/proway-computers/src/app/app.module.ts
@@ -8,19 +8,23 @@ import { FooterComponent } from './footer/footer.component';
 import { NaoEncotradaComponent } from './nao-encotrada/nao-encotrada.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule} from '@angular/material/snack-bar';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NaoEncotradaComponent
+    NaoEncotradaComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Trilha Java/05 - Angular/proway-computers/src/app/produtos/produtos.component.ts
+++ Trilha Java/05 - Angular/proway-computers/src/app/produtos/produtos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-produtos',
@@ -11,11 +13,20 @@ export class ProdutosComponent implements OnInit {
   produtos: IProduto[] | undefined;
 
   constructor(
-    private produtoService: ProdutosService
+    private produtoService: ProdutosService,
+    private route: ActivatedRoute
   ){}
 
   ngOnInit(): void{
-    this.produtos = this.produtoService.getAll();
-  }
+    const produtos = this.produtoService.getAll();
+    this.route.queryParamMap.subscribe(parans => {
+      const descricao = parans.get("descricao")?.toLowerCase();
+      if(descricao){
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return;
+      }
 
+      this.produtos = produtos;
+    });
+  }
 }
