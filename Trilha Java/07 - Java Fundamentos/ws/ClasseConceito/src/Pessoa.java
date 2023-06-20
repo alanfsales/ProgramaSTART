37d352932d343926d2353fbc4eac36c60c91b4e3
AlanFSales <alanfsales27@gmail.com>
2023-06-19 22:51:38 -0300

    Até a aula 13D Fund. Java

---
+++ Trilha Java/07 - Java Fundamentos/ws/ClasseConceito/src/Pessoa.java
@@ -0,0 +1,11 @@
+
+public class Pessoa {
+
+	public String nomePessoa;
+	public int idadePessoa;
+	
+	public void mostraDados(String pNome, int pIdade) {
+		System.out.println("Nome : " + pNome);
+		System.out.println("Idade : " + pIdade);
+	}
+}
---
+++ Trilha Java/07 - Java Fundamentos/ws/ClasseConceito/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Pessoa pes = new Pessoa();
+		pes.idadePessoa = 15;
+		pes.nomePessoa = "João";
+		pes.mostraDados(pes.nomePessoa, pes.idadePessoa);
+		
+	}
+}
